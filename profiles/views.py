5e17d95722e75845dc654d7a8f5b0486ab48b4ed
GlennOnyango <glennonyango@yahoo.com>
2024-04-22 11:59:51 +0300

    repalced View with CreateVIEW

--- profiles/views.py
+++ profiles/views.py
@@ -4,20 +4,13 @@ from django.http import HttpResponseRedirect
 from .forms import ProfileForm
 from .models import ProfileModel
 
+from django.views.generic import CreateView
+
 # Create your views here.
 
 
-class CreateProfileView(View):
-    def get(self, request):
-        profile_form = ProfileForm()
-        return render(request, "profiles/create_profile.html", {"form": profile_form})
-
-    def post(self, request):
-        submited_form = ProfileForm(request.POST, request.FILES)
-
-        if submited_form.is_valid():
-            profile_model = ProfileModel(image=request.FILES["image"])
-            profile_model.save()
-            return HttpResponseRedirect("/profiles")
-
-        return render(request, "profiles/create_profile.html", {"form": submited_form})
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = ProfileModel
+    fields = "__all__"
+    success_url = "/profiles"
